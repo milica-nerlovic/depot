5a1392d0eddf8264e833a069a87d215155ea37de
milica-nerlovic <milica.nerlovic@gmail.com>
2017-02-09 11:04:43 +0100

    Add more functional tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select ".list_description", minimum: 1
+    assert_select ".list_actions"
   end
 
   test "should get new" do
