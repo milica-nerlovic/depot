9bef32b7b7bcb7de55404892a4c85bb2d8dba6f0
milica-nerlovic <milica.nerlovic@gmail.com>
2017-02-20 11:22:43 +0100

    Add ajax around the cart operations

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,9 @@
 class StoreController < ApplicationController
   include StoreIndexVisitsCounter
+  include CurrentCart
 
   before_action :count_store_index_visits, only: [:index]
+  before_action :set_cart
 
   def index
     @products = Product.order(:title)
